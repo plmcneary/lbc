2a2bf655c81b46b4a005dee3497fc97a57aedee6
Peter McNeary <plmcneary@icloud.com>
2016-03-01 23:46:46 -0500

    Add the Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+
+  def help
+  end
+
+  def faq
+  end
+
+  def copyright
+  end
+
+  def terms
+  end
+end
