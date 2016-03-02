f25424aa1a8b2a8b2810b6839a40617c73838066
Peter McNeary <plmcneary@icloud.com>
2016-03-02 12:56:42 -0500

    Add Users controller, site_layout integration test, :users resource, signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   get 'copyright' => 'static_pages#copyright'
   get 'terms' => 'static_pages#terms'
 
+  get 'signup' => 'users#new'
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
