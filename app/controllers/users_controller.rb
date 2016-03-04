9469be45ab47c006587bb0acef923a2dd40fb9e7
Peter McNeary <plmcneary@icloud.com>
2016-03-03 19:47:30 -0500

    Add sign up error messages, sign up integration test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to Lush Bull City!"
       redirect_to @user
     else
       render 'new'
@@ -29,4 +29,10 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :organization, :address_line_1, :address_line_2, :city, :zip_code, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { first_name: "", last_name: "", email: "user@invalid", address_line_1: "222 W Hargett St", city: "Raleigh", zip_code: 27601, password: "foo", password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { first_name: "Example", last_name: "User", email: "user@example.com", organization: "Lush Bull City", address_line_1: "101 City Hall Plaza", city: "Durham", zip_code: 27701, password: "foobar", password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+  end
+end
