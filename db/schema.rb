99c4773c0747fb41ecff2ab26ffa6d13d782c485
Peter McNeary <plmcneary@icloud.com>
2016-04-09 17:37:03 -0400

    Add account activations

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,12 @@ ActiveRecord::Schema.define(version: 20160302200554) do
     t.integer  "zip_code"
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(first_name: "Administrative", last_name: "User", email: "admin@example.com", address_line_1: "101 City Hall Plaza", city: "Durham", zip_code: 27701, password: "foobar", password_confirmation: "foobar", admin: true)
+User.create!(first_name: "Administrative", last_name: "User", email: "admin@example.com", address_line_1: "101 City Hall Plaza", city: "Durham", zip_code: 27701, password: "foobar", password_confirmation: "foobar", admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   first_name  = Faker::Name.first_name
@@ -10,5 +10,5 @@ User.create!(first_name: "Administrative", last_name: "User", email: "admin@exam
   city = "Durham"
   zip_code = 27701
   password = "password"
-  User.create!(first_name: first_name, last_name: last_name, email: email, organization: organization, address_line_1: address_line_1, city: city, zip_code: zip_code, password: password, password_confirmation: password)
+  User.create!(first_name: first_name, last_name: last_name, email: email, organization: organization, address_line_1: address_line_1, city: city, zip_code: zip_code, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
