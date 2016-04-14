1d40d99865b89face136aa69f7ec58a4970f5412
Peter McNeary <plmcneary@icloud.com>
2016-04-13 23:11:22 -0400

    Fix user mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,10 +6,10 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:peter)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account Activation", mail.subject
+    assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.first_name, mail.body.encoded
+    assert_match user.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
@@ -18,10 +18,10 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:peter)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password Reset", mail.subject
+    assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.reset_token,        mail.body.encoded
+    assert_match user.reset_token, mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
 end
